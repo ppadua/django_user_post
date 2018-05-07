9434259f285b13f033c79e382d53bf8ac63e8c7c
Paul John <ppadua@village88.com>
2018-05-07 08:42:06 +0800

    Add static

--- like_facebook/urls.py
+++ like_facebook/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf.urls.static import static
 
 urlpatterns = [
 	url(r'^', include('apps.user_dashboard.urls')), 
